57069529edeb9aea4b57dff08e86b84707057300
Grim-spec135 <manjagallis@gmail.com>
2026-02-12 04:33:22 -0300

    Create calc.sh
    Create calc.sh

---
+++ calc.sh
@@ -0,0 +1,17 @@
+nome = input("digite seu nome")
+print("bem vindo:", nome)
+
+num1 = float(input("digite um número"))
+num2 = float(input("digite um número"))
+soma = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mult = num1 * num2
+porc = num1 % num2
+print(soma, "Essa é a soma de suas operações," , nome)
+print(sub, "Essa é a subtração de suas operações", nome)
+print(mult, "Essa é a multiplicação de suas operações", nome)
+print(div, "Essa é a divisão de suas operações", nome)
+print(porc, "Essa é a porcentagem de suas operações", nome)
+
+print("Obrigado por usar este script", nome)
